1ce024a7243e6876285dfba0f4a6ad32052e9f8a
eleasar123 <eleasar.patot@student.passerellesnumeriques.org>
2021-05-26 18:01:42 +0800

    Added

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const socket = require("socket.io");
+
+// App setup
+const PORT = 5000;
+const app = express();
+const server = app.listen(PORT, function() {
+    console.log(`Listening on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
+
+// Static files
+app.use(express.static("public"));
+
+// Socket setup
+const io = socket(server);
+let activeUsers = [];
+// const activeUsers = new Set();
+socket.nickname = "";
+io.on("connection", function(socket) {
+    // console.log(socket)
+    console.log("Made socket connection");
+
+    //get the user who connected
+    socket.on("user connected", function(nickname) {
+        socket.nickname = nickname;
+        // console.log(nickname)
+        if (nickname != "") {
+            activeUsers.push(nickname);
+        }
+        console.log(activeUsers + "hey");
+        io.emit("all users", activeUsers);
+        // console.log("kini siya niapil"+nickname)
+        // socket.broadcast.emit("user connected",nickname);
+        console.log("user" + activeUsers);
+        return;
+    });
+
+    socket.on("disconnect", () => {
+        // console.log(socket.nickname)
+        var i = activeUsers.indexOf(socket.nickname);
+        console.log(i + "t");
+        console.log(socket.nickname + "left oi");
+        socket.broadcast.emit("user disconnected", socket.nickname);
+        console.log(activeUsers);
+        if (i != -1) {
+            activeUsers.splice(i, 1);
+        }
+        // activeUsers.splice(i, 1);
+        console.log(activeUsers);
+    });
+
+    socket.on("user disconnected", data => {
+        var i = activeUsers.indexOf(data);
+        console.log(data + " left");
+        socket.broadcast.emit("user disconnected", data);
+        if (i != -1) {
+            activeUsers.splice(i, 1);
+        }
+        // activeUsers.splice(i, 1);
+        console.log(activeUsers);
+    });
+
+    socket.on("chat message", function(nickname, data) {
+        console.log(nickname + " says " + data);
+        socket.broadcast.emit("chat message", nickname, data);
+    });
+
+    socket.on("typing", function(data) {
+        console.log(data.nick + " is typing");
+        socket.broadcast.emit("typing", data.nick);
+    });
+
+    socket.on("stopped typing", function(data) {
+        console.log(data + " stopped typing");
+        socket.broadcast.emit("stopped typing", data);
+    });
+
+    socket.on("private message", function(receiverName, senderName, input) {
+        console.log(receiverName + " " + senderName + " " + input);
+        socket.broadcast.emit("private message", receiverName, senderName, input);
+    });
+});
---
+++ public/main.js
@@ -0,0 +1,289 @@
+const socket = io();
+
+var date = new Date();
+const formattedTime = date.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric"
+});
+
+var form = document.getElementById("chat-form");
+var input = document.getElementById("input");
+var messages = document.getElementById("message-text");
+var nickname = "";
+
+$(".joinChat").click(function() {
+    nickname = $(this)
+        .parent()
+        .siblings(".nicknameDiv")
+        .children("#nickname")
+        .val();
+    console.log(nickname);
+
+    if (nickname != "") {
+        socket.emit("user connected", nickname);
+        // socket.emit("new user", nickname);
+        $("#userJoined").html("You joined the chat!");
+        $(".nickname").hide();
+        window.setTimeout(function() {
+            $(".chat").show();
+        }, 300);
+        $("#you").html(
+            `<div class="conversation active2" id="' ${nickname} '"><i class="fa fa-user-circle-o" aria-hidden="true" style="color:greenyellow;">Active</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${nickname}</div>`
+        );
+    } else {
+        // $(this).parent().siblings('.nicknameDiv').children('#nickname').val("Enter a nickname")
+        $("#nickname").attr("placeholder", "Enter a nickname");
+    }
+});
+
+$("#back").click(function() {
+    console.log(nickname + " hello");
+    socket.emit("user disconnected", nickname);
+    $(".chat").hide();
+    window.setTimeout(function() {
+        $(".nickname").show();
+    }, 300);
+});
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    if (input.value) {
+        console.log(nickname + input.value);
+        console.log(typeof input.value);
+        socket.emit("chat message", nickname, input.value);
+        var item1 = document.createElement("div");
+        item1.classList.add("paraRight");
+        item1.innerHTML =
+            "<strong>You: </strong>" +
+            input.value +
+            "<br><span><em>" +
+            formattedTime +
+            "</em></span>";
+        messages.append(item1);
+        input.value = "";
+    }
+});
+
+input.addEventListener("keydown", () => {
+    console.log(nickname);
+    socket.emit("typing", {
+        isTyping: input.value.length > 0,
+        nick: nickname
+    });
+});
+
+input.addEventListener("keyup", () => {
+    console.log(nickname);
+    socket.emit("stopped typing", nickname);
+});
+
+// socket.on("user connected", nickname => {
+//     console.log(nickname);
+//     $("#userJoined").html(`${nickname} joined the chat!`);
+//     $(".participants")
+//         .append(`<div class="conversation active" id="  ${nickname}-user"><i class="fa fa-user-circle-o" aria-hidden="true" style="color:greenyellow;">Active</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${nickname}&nbsp;&nbsp;&nbsp;&nbsp;<button class="title-text btn users" id="${nickname}'"><i class="fas fa-envelope"></i>PM</button></div>
+//     <div class="conversation-message" id="${nickname}-privateMessage">No message for you oi</div>
+//     <div class="card " id="${nickname}-messageBox" style="display: none;position:absolute;font-size:15px;width:45rem;height:40rem;border:solid black 2px;bottom:30px;left:40%;">
+//         <div class="container-fluid">    
+//                 <span class="pull-left receiverName" id="${nickname}-receiverName" style="width:fit-content;"></span>
+//                 <button class="btn btn-danger pull-right" id="${nickname}-exit"style="margin:5px;">X</button>
+//             </div>
+//         <div class="container" id="${nickname}-message-text" style="height:80%;width:95%;border:solid blue 1px;margin:10px;overflow-y: scroll;">
+
+//         </div>
+//         <div class="container p-1 m-1">
+//             <form id="${nickname}-form" class="form ">
+//             <button class="pull-left" style="border:none; width:fit-content;height:auto "><i class="fa fa-send-o" style="font-size:15px;margin-right:10px"
+//             alt="Send"></i>Send</button>
+//             <input id="${nickname}-input" autocomplete="off"class="form-control pull-right" style="font-size:15px;width:80%;height:auto" placeholder="Type a message" /><br>
+//         </div> 
+//     </div>
+//     `);
+
+//     $(document).on("click", "#" + nickname, function() {
+//         console.log(nickname);
+//         let receiverName = this.id;
+//         let senderName = nickname;
+
+//         $("#" + receiverName + "-messageBox").show();
+//         $("#" + receiverName + "-receiverName").html(receiverName);
+//         $("#" + receiverName + "-receiverName").css({
+//             "margin-left": "10%",
+//             "padding-left": "30px",
+//             "padding-right": "30px",
+//             "text-align": "center",
+//             "width": "fit-content",
+//             "font-weight": "bold",
+//             "font-size": "25px",
+//             "background-color": "skyblue",
+//             "border": "solid black green 2px",
+//             "border-radius": "5px"
+//         })
+//         $("#" + receiverName + "-exit").on("click", function() {
+//             $("#" + receiverName + "-messageBox").hide();
+//         });
+//         $("#" + receiverName + "-form").submit(function(e) {
+//             e.preventDefault();
+//             let input = $("#" + receiverName + "-input").val();
+//             if (input) {
+//                 socket.emit("private message", receiverName, senderName, input);
+//                 var item1 = document.createElement("div");
+//                 item1.classList.add("paraRight");
+//                 item1.innerHTML =
+//                     "<strong>You: </strong>" +
+//                     input +
+//                     "<br><span><em>" +
+//                     formattedTime +
+//                     "</em></span>";
+//                 $("#" + receiverName + "-message-text").append(item1);
+//                 $("#" + receiverName + "-input").val("");
+//             }
+//         });
+//     });
+// });
+
+socket.on("user disconnected", function(nickname) {
+    $("#userJoined").html(nickname + " left the chat!");
+    $("#" + nickname + "-privateMessage").remove();
+
+    $("#" + nickname + "-user").remove();
+
+    // $('#'+nickname).children('.fa-user-circle-o').css("color","gray");
+    // $('#'+nickname).remove();
+});
+
+socket.on("typing", function(data) {
+    $("#typing").html(data + " is typing");
+});
+
+socket.on("stopped typing", function(data) {
+    $("#typing").html(data + " stopped typing");
+    setTimeout(function() {
+        $("#typing").html("Nobody is typing");
+    }, 1000);
+});
+
+socket.on("chat message", function(nickname, data) {
+    $("#message-text").append(
+        '<div class="" style="margin-left:0;margin-right:40%"><p><strong>' +
+        nickname +
+        ": </strong>" +
+        data +
+        "</p><em>" +
+        formattedTime +
+        "<em></div><br>"
+    );
+});
+
+socket.on("all users", function(users) {
+    users.forEach(element => console.log(element));
+    document.querySelector(".participants").innerHTML = "";
+    for (i = 0; i < users.length; i++) {
+        if (users[i] === nickname) {
+            continue;
+        }
+        $(".participants")
+            .append(`<div class="conversation active" id="${users[i]}-user"><i class="fa fa-user-circle-o" aria-hidden="true" style="color:greenyellow;">Active</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${users[i]}&nbsp;&nbsp;&nbsp;&nbsp;<button class="title-text btn user" id="${users[i]}"><i class="fas fa-envelope"></i>PM</button></div>
+        <div class="conversation-message" id="${users[i]}-privateMessage">No message for you</div>
+        <div class="card " id="${users[i]}-messageBox" style="display:none;position:absolute;font-size:15px;width:45rem;height:40rem;border:solid black 2px;bottom:50px;left:40%;">
+            <div class="container-fluid">    
+                <span class="pull-left receiverName" id="${users[i]}-receiverName" style="width:fit-content;"></span>
+                <button class="btn btn-danger pull-right" id="${users[i]}-exit"style="margin:5px;">X</button>
+            </div>
+            <div class="container" id="${users[i]}-message-text" style="height:80%;width:95%;border:solid blue 1px;overflow-y: scroll;">
+            
+            </div>
+            <div class="container p-1 m-1">
+                <form id="${users[i]}-form" class="form ">
+                <button class="pull-left" style="border:none; width:fit-content;height:auto"><i class="fa fa-send-o" style="font-size:15px;margin-right:10px"
+                        alt="Send"></i>Send</button>
+                     <input id="${users[i]}-input" class="form-control pull-right" style="width:80%;height:auto" style="font-size:15px;" autocomplete="off" placeholder="Type a message" />
+            </div>
+            </div><br>
+        `);
+
+        $(document).on("click", "#" + users[i], function() {
+            console.log(nickname);
+            let receiverName = this.id;
+            let senderName = nickname;
+
+            $("#" + receiverName + "-messageBox").show();
+            $("#" + receiverName + "-receiverName").html(receiverName);
+            $("#" + receiverName + "-receiverName").css({
+                "margin-left": "10%",
+                "padding-top": "5px",
+                "padding-bottom": "5px",
+                "padding-left": "30px",
+                "padding-right": "30px",
+                "text-align": "center",
+                "width": "fit-content",
+                "font-weight": "bold",
+                "font-size": "25px",
+                "background-color": "skyblue",
+                "border": "solid black green 2px",
+                "border-radius": "5px"
+            })
+            $("#" + receiverName + "-exit").on("click", function() {
+                $("#" + receiverName + "-messageBox").hide();
+            });
+            $(document).on("submit", "#" + receiverName + "-form", function(e) {
+                e.preventDefault();
+                let input = $("#" + receiverName + "-input").val();
+                if ($(input) != "") {
+                    socket.emit("private message", receiverName, senderName, input);
+                    var item1 = document.createElement("div");
+                    item1.classList.add("paraRight");
+                    item1.innerHTML =
+                        "<strong>You: </strong>" +
+                        input +
+                        "<br><span><em>" +
+                        formattedTime +
+                        "</em></span>";
+                    $("#" + receiverName + "-message-text").append(item1);
+                    $("#" + receiverName + "-input").val("");
+                }
+            });
+        });
+    }
+});
+
+//for emoji
+$(".emoji").on("click", function() {
+    // console.log('clicked emoji'+this.value)
+    // console.log(this.value)
+    let inputValue = $("#input").val();
+    $("#input").val(inputValue + this.value);
+    console.log($("#input").val(inputValue + this.value));
+});
+
+//for search bar
+$("#search").on("keyup", function() {
+    console.log("key up");
+    // let input=$('#search').val().toUpperCase();
+    var value = $(this).val().toLowerCase();
+    $(".conversation").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+});
+
+$("#exit").on("click", function() {
+    $(this).parents("#privateMessage").hide();
+});
+
+//for private message
+
+socket.on("private message", function(receiverName, senderName, input) {
+    if (nickname === receiverName) {
+        console.log(receiverName + " " + senderName + " " + input);
+        $("#" + senderName + "-privateMessage").html(senderName + " says " + input);
+        $("#" + senderName + "-message-text").append(
+            '<div class="" style="margin-left:0;font-size:15px;color:black;margin-right:0%"><p><strong>' +
+            senderName +
+            ": </strong>" +
+            input +
+            "</p><em>" +
+            formattedTime +
+            "<em></div><br>"
+        );
+    }
+});
